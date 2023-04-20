9962ca6f7cf0bf7089da5232b5296110512c03f2
Josiah Greenwell <jsgreenwell@users.noreply.github.com>
2023-04-20 13:32:02 -0400

    Update linear_duplicates.py

--- leetcodetricks/linear_duplicates.py
+++ leetcodetricks/linear_duplicates.py
@@ -7,9 +7,37 @@ So duplicates could use a brute-force method which is basically a bubble sort:
    
 If you see a double for-loop - its "bad-code smell" (not nesscarily wrong but typically inefficient)
 
-Second version shown in checkSet: convert to set (sets have to be unique values) and see if same length as list
+Second version shown is what I see from many applicatants (trying to be Pythonic) - its not just uses some built-ins
+   Without thinking about O() notation: why is this only a little more memory efficient and faster than the brute force?
+       If helps (0 and 1 elements cannot be faster than just returning).
+       Count the number of times it has to loop over values - there are 3
+
+Third version shown is checkSet: convert to set (sets have to be unique values) and see if same length as list
    if it is: return False (no dup) if its not: return True (at least 1 dup)
+   This is much faster and efficient then brute force but a set lookup is around O(1) so ...can get a bit better
+   Try and figure out why the last version is faster (its still a set and if I needed the duplicates for stuff I'd use this)
+   
+Use a Counter (either the collection tool or make your own with normal dictionary)
 """
+
+def bruteForce (n_list: List[int]) -> bool:
+        # Using a list slice to ignore first number in 2nd for
+        for i in n_list:
+            for j in n_list[1:]:
+               if i == j: return True
+        return False
+
+   
+def fancierNotFaster(self, n_list: List[int]) -> bool:
+        # Yep this helps: ignores a few cases
+        if len(n_list) <= 1:
+            # if empty or only 1 element cannot have dups
+            return False
+
+        # wonder if I can use Counter - yep, horrible though
+        return any([i-1 for i in Counter(n_list).values()])
+
+      
 def checkSet(n_list: List[int]) -> bool:
         # if empty list or only 1 element we know no dups:
         if len(n_list) <= 1:
